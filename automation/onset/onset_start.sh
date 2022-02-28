5b57ae368d7ae6c2cb6f8ed5316f04b0ce54fa73
adenisiewicz <adenisiewicz@interia.pl>
2022-02-28 10:56:42 +0100

    Add files via upload

--- automation/onset/onset_start.sh
+++ automation/onset/onset_start.sh
@@ -1,11 +1,11 @@
 czas=$(date +%s%N | cut -b1-16)
 echo $czas
-sed -i 's/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/'$czas'/g' onset_event
-cat onset_event
+sed -i 's/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/'$czas'/g' onset_event_start
+cat onset_event_start
 
 curl https://portal.api.simpledemo.onap.org:30226/events/unauthenticated.DCAE_CL_OUTPUT/?timeout=5000 -i -k -X POST\
   --header 'accept: application/json' \
   --header 'cache-control: no-cache' \
   --header 'Content-type: application/json' \
- -d '@onset_event'
+ -d '@onset_event_start'
 
