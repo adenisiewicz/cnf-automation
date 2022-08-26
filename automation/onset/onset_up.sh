49bd639dfcd9018e7860edcd04a6fab1428776c2
adenisiewicz <adenisiewicz@interia.pl>
2022-08-26 12:54:16 +0200

    onset up down

---
+++ automation/onset/onset_up.sh
@@ -0,0 +1,12 @@
+czas=$(date +%s%N | cut -b1-16)
+echo $czas
+sed -i 's/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/'$czas'/g' onset_event_up
+cat onset_event_up
+
+curl https://portal.api.simpledemo.onap.org:30226/events/unauthenticated.DCAE_CL_OUTPUT/?timeout=5000 -i -k -X POST\
+  --header 'accept: application/json' \
+  --header 'cache-control: no-cache' \
+  --header 'Content-type: application/json' \
+ -d '@onset_event_up'
+
+
